5c69777f371a8964094e6e2303ddccfb13a47c3c
miro <amirer21@gmail.com>
2023-02-06 22:53:16 +0900

    'urls

---
+++ myapp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+#클라이언트로 정보를 전달하기 위함 함수
+#파라미터 : 요청
+def index(requset):
+    return HttpResponse('Welcome!')
+
+def create(requset):
+    return HttpResponse('Create!')
+
+def read(requset, id):
+    return HttpResponse('Read!'+ id)
